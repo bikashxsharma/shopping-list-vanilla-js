d3fc667df7ced60419880d3fb548500d1cf03cab
Bikash <36207673+bikashxsharma@users.noreply.github.com>
2020-09-14 14:07:45 +0300

    text and styles

--- app.js
+++ app.js
@@ -3,34 +3,39 @@ const items = [
     id: 1,
     name: "Food twenty 5",
     price: 5.50,
-    description: "This is made of silver"
+    description: "This is made of silver",
+    qty: 0,
   },
   {
     id: 2,
     name: "Chopping Samosa ",
     price: 15.50,
-    description: "Biscuits are the good goose"
+    description: "Biscuits are the good goose",
+    qty: 0,
 
   },
   {
     id: 3,
     name: "Happy sandwich",
     price: 9.80,
-    description: "Susmando tu rages chess"
+    description: "Susmando tu rages chess",
+    qty: 0,
 
   },
   {
     id: 4,
     name: "Japarta Hultan",
     price: 11.20,
-    description: "Suskchyangtu haklal"
+    description: "Suskchyangtu haklal",
+    qty: 0,
 
   },
   {
     id: 5,
     name: "Hawai safari",
     price: 51.50,
-    description: "Luxurious food in the go"
+    description: "Luxurious food in the go",
+    qty: 0,
 
   },
 
@@ -53,8 +58,7 @@ const createItem = (item) => {
               <div class="price">
                   ${item.price} €
               </div>
-              <button id='add-to-cart' class="addItemToCart" data-id=${item.id}>Add to cart</button>`;
-
+              <button id='add-to-cart' ${item.qty >= 1 && 'disabled'}  class="addItemToCart" data-id=${item.id}>Add to cart</button>`;
   }
   return itemDiv;
 }
@@ -82,6 +86,10 @@ addToCartBtns.forEach(btn => {
 // add items to cart
 function addItemToCart(id) {
   const filteredItem = items.filter(item => item.id.toString() === id);
+  // increase Item quantity
+  filteredItem[0].qty = filteredItem[0].qty
+  console.log(filteredItem[0].qty)
+
   const itemDiv = document.createElement('div')
   itemDiv.className = `itemcart_item fade-in cart_item_id-${filteredItem[0].id}`
   itemDiv.innerHTML = ` <h3>${filteredItem[0].name}</h3>
@@ -89,6 +97,8 @@ function addItemToCart(id) {
          <button class="removeItemFromCart" data-id=${filteredItem[0].id} price=${filteredItem[0].price}> <i class="fas fa-trash-alt" ></i></button>`;
   cartLists.appendChild(itemDiv)
   totalPrice.innerText = getSum(filteredItem[0].price)
+
+  //dynamic button to remove item from the cart  
   const removeFromCartBtns = document.querySelectorAll(".removeItemFromCart");
   if (removeFromCartBtns.length > 0) {
     removeFromCartBtns.forEach(btn => {
